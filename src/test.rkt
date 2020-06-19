b6fc4ef1f1e71c4503033c758b1fd5be860bb6bf
vdblm <balazadehvahid@gmail.com>
2020-06-20 00:10:52 +0430

    new tests added

--- src/test.rkt
+++ src/test.rkt
@@ -7,30 +7,30 @@
 (define test-cases
   (test-suite "tests"
               (test-case "if-else"
-                         (check-equal? (evaluate "../samples/test-if-else.txt")
+                         (check-equal? (evaluate "../tests/test-if-else.txt")
                                     '(4 3 45)))
               (test-case "assignment"
-                         (check-equal? (evaluate "../samples/test-assignment.txt")
+                         (check-equal? (evaluate "../tests/test-assignment.txt")
                                        2))
 ;              (test-case "arithmatic")
                (test-case "list"
-                          (check-equal? (evaluate "../samples/test-list.txt")
+                          (check-equal? (evaluate "../tests/test-list.txt")
                                        2))
 ;              (test-case "while")
                (test-case "sample0"
-                          (check-equal? (evaluate "../samples/test-sample0.txt")
+                          (check-equal? (evaluate "../tests/test-sample0.txt")
                                        '(2 2)))               
                (test-case "sample1"
-                          (check-equal? (evaluate "../samples/test-sample1.txt")
+                          (check-equal? (evaluate "../tests/test-sample1.txt")
                                        5))
                (test-case "sample2"
-                          (check-equal? (evaluate "../samples/test-sample2.txt")
+                          (check-equal? (evaluate "../tests/test-sample2.txt")
                                        #t))
                (test-case "sample3"
-                          (check-equal? (evaluate "../samples/test-sample3.txt")
+                          (check-equal? (evaluate "../tests/test-sample3.txt")
                                        '(7 7/2 7/3 7/5 1 55)))
                (test-case "sample4"
-                          (check-equal? (evaluate "../samples/test-sample4.txt")
+                          (check-equal? (evaluate "../tests/test-sample4.txt")
                                        35))
               ))
 
